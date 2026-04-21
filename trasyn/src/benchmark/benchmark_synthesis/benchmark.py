09cbff3a032f484fc373d6526b09cfb199191856
Christoph Fleckenstein <c.fleckenstein@parityqc.com>
2026-04-21 11:50:38 +0200

    fixes assets path

--- trasyn/src/benchmark/benchmark_synthesis/benchmark.py
+++ trasyn/src/benchmark/benchmark_synthesis/benchmark.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     benchmark_on_random_unitaries(
         unitaries=unitaries,
         budgets=np.arange(min_budget, max_budget, 0.5),
-        load_dir="../../assets/" + gate_set_cost_25,
+        load_dir="../../../assets/" + gate_set_cost_25,
         save_dir=save_dir + "/" + gate_set_cost_25,
         seed=SEED,
         costs={"t": 1.0, "q": 2.5},
